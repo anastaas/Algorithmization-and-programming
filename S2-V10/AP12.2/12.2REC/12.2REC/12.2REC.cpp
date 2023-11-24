7d43fa7d65159d7e6ef1269988c79adf2f95496f
Anastas Goranov <148644426+anastaas@users.noreply.github.com>
2023-11-24 11:37:55 +0200

    Add files via upload

---
+++ S2-V10/AP12.2/12.2REC/12.2REC/12.2REC.cpp
@@ -0,0 +1,95 @@
+﻿// 12.2REC.cpp
+// < Горанов Анастас >
+// Лабораторна робота № 12.2REC
+// Опрацювання лінійного однонаправленого списку
+// Варіант 10
+
+#include <iostream>
+
+using namespace std;
+
+typedef int Info; // створення псевдоніму типу
+
+struct Elem
+{
+	Elem* link;
+	Info info;
+};
+
+void enqueue(Elem*& first, Elem*& last, Info k, Info numb);
+void Print(Elem* L);
+void del(Elem* L, Info number);
+void dequeue(Elem*& first, Elem*& last);
+
+int main()
+{
+
+	Elem* first = NULL,
+		* last = NULL;
+	enqueue(first, last, 1, 15);
+	Print(first); // Вивід
+	cout << endl;
+
+	Info number;
+	cout << "Enter a number after which all items will be deleted: "; cin >> number;
+
+	del(first, number); // видалення компонентів 
+
+	dequeue(first, last); // Вивід
+	cout << endl;
+
+
+	return 0;
+}
+void enqueue(Elem*& first, Elem*& last, Info k, Info numb)
+{
+	Elem* tmp = new Elem;
+	tmp->info = k;
+	tmp->link = NULL;
+	if (last != NULL)
+		last->link = tmp;
+	last = tmp;
+	if (first == NULL)
+		first = tmp;
+	if (k < numb) {
+		k++;
+		enqueue(first, last, k, numb);
+	}
+}
+
+void dequeue(Elem*& first, Elem*& last)
+{
+	Elem* tmp = first->link;
+	Info value = first->info;
+	delete first;
+	first = tmp;
+	if (first == NULL)
+		last = NULL;
+	cout << value << " ";
+	if (last != NULL) {
+		dequeue(first, last);
+	}
+}
+
+void Print(Elem* L)
+{
+	if (L != NULL) {
+		cout << L->info << " ";
+		L = L->link;
+		Print(L);
+	}
+}
+
+void del(Elem* L, Info number)
+{
+	if (L != NULL && L->link != NULL) {
+		if (L->link->info > number) {
+			Elem* tmp = L->link->link;
+			delete L->link;
+			L->link = tmp;
+			del(L, number);
+		}
+		else
+			del(L->link, number);
+	}
+}
---
+++ S2-V10/AP12.2/12.2REC/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\12.2REC\12.2REC.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
