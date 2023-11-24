1f2f9532f614c3b40ccab60c7deb5f08b344b18b
Anastas Goranov <148644426+anastaas@users.noreply.github.com>
2023-11-24 11:55:55 +0200

    Add files via upload

---
+++ S2-V10/AP8.2/8.2/8.2/8.2.cpp
@@ -0,0 +1,39 @@
+﻿// 8.2.cpp
+// < Горанов Анастас >
+// Лабораторна робота № 8.2
+// Опрацювання літерних рядків
+// Варіант 10
+
+#include <string.h>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+bool FirstLetter(char* str);
+
+int main()
+{
+    char s[101];
+    cout << "Enter string:" << endl;
+    cin.getline(s, 101);
+    if (FirstLetter(s))
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+bool FirstLetter(char* str)
+{
+    int k = 0;
+    char* nextToken;
+    char* str2 = strtok_s(str, " ", &nextToken);
+    while (str2 != NULL)
+    {
+        if (str2[0] == 'a')
+            return true;
+
+        str2 = strtok_s(NULL, " ", &nextToken);
+    }
+
+    return false;
+}
---
+++ S2-V10/AP8.2/8.2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,37 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\8.2\8.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestMethod1)
+        {
+            char str[] = "anastas goranov";
+            bool expected = true;
+            bool result = FirstLetter(str);
+            Assert::AreEqual(result, expected);
+        }
+
+        TEST_METHOD(TestMethod2)
+        {
+            char str[] = "goranov anastas";
+            bool expected = true;
+            bool result = FirstLetter(str);
+            Assert::AreEqual(result, expected);
+        }
+
+        TEST_METHOD(TestMethod3)
+        {
+            char str[] = "goranov goranov";
+            bool expected = false;
+            bool result = FirstLetter(str);
+            Assert::AreEqual(result, expected);
+        }
+	};
+}
