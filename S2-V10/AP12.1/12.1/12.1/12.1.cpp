86815ca7978d9b33fd879c4d185c864613a59090
Anastas Goranov <148644426+anastaas@users.noreply.github.com>
2023-11-24 11:39:20 +0200

    Add files via upload

---
+++ S2-V10/AP12.1/12.1/12.1/12.1.cpp
@@ -0,0 +1,49 @@
+﻿// 12.1.cpp
+// < Горанов Анастас >
+// Лабораторна робота № 12.1
+// Формування і знищення динамічної мережної структури даних
+// Варіант 10
+
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+struct Elem
+{
+    Elem* link1,
+        * link2;
+    int info;
+};
+
+int main()
+{
+    Elem* p;
+    p = new Elem;
+
+    p->info = 1;
+    p->link1 = new Elem;
+    p->link1->info = 2;
+
+    p->link1->link1 = p;
+    p->link1->link2 = new Elem;
+    p->link1->link2->info = 3;
+
+    p->link1->link2->link1 = p->link1;
+    p->link2 = new Elem;
+    p->link2->info = 4;
+
+    p->link2->link1 = p->link1;
+    p->link2->link2 = NULL;
+    p->link1->link2->link1 = p->link2;
+    p->link1->link2->link2 = p->link2;
+
+    cout << (p->info) << endl;
+    cout << (p->link1->info) << endl;
+    cout << (p->link1->link2->info) << endl;
+    cout << (p->link2->info) << endl;
+
+    delete p->link2;
+    delete p->link1->link2;
+    delete p->link1;
+    delete p;
+}
---
+++ S2-V10/AP12.1/12.1/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\12.1\12.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+            Elem* p = new Elem;
+
+            p->info = 1;
+            p->link1 = new Elem;
+            p->link1->info = 2;
+
+            p->link1->link1 = p;
+            p->link1->link2 = new Elem;
+            p->link1->link2->info = 3;
+
+            // Чек p
+            assert(p->info == 1);
+            assert(p->link1->info == 2);
+
+            // Чек p->link1
+            assert(p->link1->link1 == p);
+            assert(p->link1->link2->info == 3);
+
+            // Клін
+            delete p->link1->link2;
+            delete p->link1;
+            delete p;    
+		}
+	};
+}
